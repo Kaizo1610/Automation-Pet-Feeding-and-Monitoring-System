3206e04eaade6184b0cbfc26f714df810f698905
aimanakim <mohamadaiman896@gmail.com>
2024-10-18 18:38:23 +0800

    y

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,22 @@
 import { Stack } from "expo-router";
 import { useFonts } from "expo-font";
+import AppLoading from 'expo-app-loading';
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    'outfit': require('./../assets/fonts/Outfit-Regular.ttf'),
+    'outfit-medium': require('./../assets/fonts/Outfit-Medium.ttf'),
+    'outfit-bold': require('./../assets/fonts/Outfit-Bold.ttf'),
+    'kaushan': require('./../assets/fonts/KaushanScript-Regular.ttf'),
+  });
 
-  useFonts({
-    'outfit':require('./../assets/fonts/Outfit-Regular.ttf'),
-    'outfit-medium':require('./../assets/fonts/Outfit-Medium.ttf'),
-    'outfit-bold':require('./../assets/fonts/Outfit-Bold.ttf'),
-    'kaushan':require('./../assets/fonts/KaushanScript-Regular.ttf'),
-  })
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
 
   return (
     <Stack screenOptions={{
-      headerShown:false
+      headerShown: false
     }}>
       <Stack.Screen name="index" />
     </Stack>
